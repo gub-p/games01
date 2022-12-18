9d82861135eb6000395f34a73f3100cf2b13ca41
BennyGutmann <bernhard.gutmann@baslerhofmann.ch>
2022-12-18 18:16:31 +0100

    wip

---
+++ HeroBorn/Assets/scripts/LearningCourve.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCourve : MonoBehaviour
+{
+
+    public int CurrentAge = 30;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(30 + 1);
+        Debug.Log(CurrentAge + 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SuperChandra/Assets/Scripts/Chandra.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chandra : MonoBehaviour
+{
+    private Rigidbody2D f_rigidBody2D;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
